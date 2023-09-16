cd1155e12a1254c6424ec5ea2836a85364469300
Deepacks <vladimirsette.vc@gmail.com>
2023-09-16 23:15:24 +0200

    add riverpod

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'views/home_view.dart';
 
 void main() {
   Paint.enableDithering = true;
-  runApp(const MyApp());
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
