3608b5364593db7f8f47cd9a2e67b6ce4cdfa1cc
Deepacks <vladimirsette.vc@gmail.com>
2023-10-27 16:54:54 +0200

    add user name in greeting

--- lib/widgets/home/greeting.dart
+++ lib/widgets/home/greeting.dart
@@ -1,23 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:spotiflutter/web/models/user.dart';
 
-class Greeting extends StatelessWidget {
+import '../../providers/secure_storage_provider.dart';
+import '../../providers/spotify_remote_provider.dart';
+
+class Greeting extends ConsumerStatefulWidget {
   const Greeting({super.key});
+
+  @override
+  ConsumerState<Greeting> createState() => _GreetingState();
+}
+
+class _GreetingState extends ConsumerState<Greeting> {
+  User? user;
+
+  void fetchUser() async {
+    final secureStorage = ref.read(secureStorageProvider);
+    final token = await secureStorage.read(key: 'token');
+
+    final spotifyRemote = ref.read(spotifyRemoteProvider);
+
+    user = await spotifyRemote.webApi.userApi.getCurrentUserProfile(token!);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     final DateTime now = DateTime.now();
     final int currentHour = now.hour;
 
-    String greeting = '';
+    String greeting = ', ${user?.displayName}';
 
     if (currentHour >= 6 && currentHour < 13) {
-      greeting = 'Good morning';
+      greeting = 'Good morning$greeting';
     } else if (currentHour >= 13 && currentHour < 18) {
-      greeting = 'Good afternoon';
+      greeting = 'Good afternoon$greeting';
     } else if (currentHour >= 18 && currentHour < 23) {
-      greeting = 'Good evening';
+      greeting = 'Good evening$greeting';
     } else {
-      greeting = 'Good night';
+      greeting = 'Good night$greeting';
     }
 
     return Text(
