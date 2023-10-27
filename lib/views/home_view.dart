b8d118f7943aa04eb457c0ef85f7f87a420baf1b
Deepacks <vladimirsette.vc@gmail.com>
2023-10-27 16:54:33 +0200

    add token secure storage

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -3,6 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../providers/connection_state_provider.dart';
 import '../providers/spotify_remote_provider.dart';
+import '../providers/secure_storage_provider.dart';
 import '../widgets/gradient_container.dart';
 import '../widgets/home/home.dart';
 // import '../widgets/home/navbar.dart';
@@ -16,12 +17,33 @@ class HomeView extends ConsumerStatefulWidget {
 }
 
 class HomeViewState extends ConsumerState<HomeView> {
+  // int currentPageIndex = 0;
+  // void onDestinationSelected(int pageIndex) {
+  //   setState(() {
+  //     currentPageIndex = pageIndex;
+  //   });
+  // }
+
   @override
   void didChangeDependencies() async {
     final spotifyRemote = ref.read(spotifyRemoteProvider);
     final connectionState = ref.read(connectionStateProvider);
 
     if (!connectionState.connected) {
+      final secureStorage = ref.read(secureStorageProvider);
+
+      final storedToken = await secureStorage.read(key: 'token');
+
+      if (storedToken == null) {
+        final token = await spotifyRemote.getAccessToken(
+          '75976a3f1c984aaa9eb0d9cb7df73a6d',
+          'http://com.bytemeup.spotify_carbon',
+          'user-read-playback-state,user-modify-playback-state,user-read-currently-playing,playlist-read-private,user-top-read,user-read-recently-played,user-library-modify,user-library-read',
+        );
+
+        await secureStorage.write(key: 'token', value: token);
+      }
+
       await spotifyRemote.connectToSpotify(
         '75976a3f1c984aaa9eb0d9cb7df73a6d',
         'http://com.bytemeup.spotify_carbon',
@@ -29,13 +51,6 @@ class HomeViewState extends ConsumerState<HomeView> {
     }
 
     super.didChangeDependencies();
-  }
-
-  int currentPageIndex = 0;
-  void onDestinationSelected(int pageIndex) {
-    setState(() {
-      currentPageIndex = pageIndex;
-    });
   }
 
   @override
--- lib/widgets/home/home.dart
+++ lib/widgets/home/home.dart
@@ -1,20 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:spotify_carbon/providers/connection_state_provider.dart';
 
 import 'greeting.dart';
 
-class Home extends StatelessWidget {
+class Home extends ConsumerWidget {
   const Home({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final connectionState = ref.watch(connectionStateProvider);
+
     return ListView(
       padding: const EdgeInsets.symmetric(horizontal: 20),
       children: [
         const SizedBox(height: 20),
-        Greeting(),
+        const Greeting(),
         const SizedBox(
           height: 100,
-        )
+        ),
+        ...(connectionState.connected ? [const Placeholder()] : []),
       ],
     );
   }
